52268575658a593ef7a7592f85e5c4377ddc67ae
RE-NY <rksamadkhani@gmail.com>
2024-06-05 17:00:20 +0530

    utility file added

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import os
+
+def code_reader_func(file_name):
+    path = os.path.join("data", file_name)
+    try:
+        with open(path, "r") as f:
+            content = f.read()
+            return {"file_content": content}
+    except Exception as e:
+        return {"error": str(e)}
